c923a78be001b8c9c964247375836ad0ddab1500
slava bily <slavabily@me.com>
2021-11-17 09:10:10 +0200

    the array of dividers for upper limit is obtained

--- BetweenTwoSets/main.swift
+++ BetweenTwoSets/main.swift
@@ -8,9 +8,23 @@
 import Foundation
 
 func getTotalX(a: [Int], b: [Int]) -> Int {
-    // Write your code here
-    return 2
+    var bA = [Int]()
+    for i in 1...100 {
+        var b1 = [Int]()
+        for j in 0..<b.count {
+            if b[j] % i == 0 {
+                b1.append(i)
+            }
+        }
+        if b1.count > 1 {
+            let bS = Set(b1)
+            print(bS)
+            bA = bA + bS
+        } 
+    }
+    print(bA)
+    return 0
 }
 
-print(getTotalX(a: [2,6], b: [24,35]))
+print(getTotalX(a: [2,6], b: [24,36]))
 
